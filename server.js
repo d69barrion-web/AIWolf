151b3c6c4e92bc5fa4e4cfc7278757d6773ca442
DANIEL BARRION <d69barrion@gmail.com>
2026-09-18 17:03:38 +0800

    Update server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ initializeFirebase();
 // TEST MODE:
 // true  = hindi tatawag sa OpenAI API
 // false = tunay na AIWolf / OpenAI response
-const AIWOLF_TEST_MODE = true;
+const AIWOLF_TEST_MODE = false;
 
 // Maximum AIWolf requests per visitor
 const AIWOLF_LIMIT = 10;
